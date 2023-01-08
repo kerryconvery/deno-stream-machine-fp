f17eaed3d06ecbc373419adbe270ee03224a32eb
DESKTOP-UE92379\Kerry <kerry.convery@myob.com>
2023-01-08 21:48:14 +1100

    will replace getStreams service will getAllStreams service

--- route-handlers/streams_handlers.ts
+++ route-handlers/streams_handlers.ts
@@ -4,15 +4,10 @@ import { AggregatedStreams } from "../stream-mappers/types.ts";
 import { TwitchHelixGateway } from "../streaming-platform-gateways/twitch_helix_gateway.ts";
 
 export function getStreamsHandler(): Promise<AggregatedStreams> {
-  const twichGateway = new TwitchHelixGateway(
-    "https://id.twitch.tv",
-    "https://api.twitch.tv",
-    "hmimgefw1924xe48uk2fhmuq1tnvfs",
-    "tb8c2twoljtdbjvz6josstvie2iwl2"
-  );
+  const twichGateway = new TwitchHelixGateway("", "", "", "");
 
   const twitchStreamsA = maybeGetTwitchStreams(twichGateway);
-  // const twitchStreamsB = maybeGetTwitchStreams(twichGateway);
+  const twitchStreamsB = maybeGetTwitchStreams(twichGateway);
 
-  return getAllStreams(twitchStreamsA);
+  return getAllStreams(twitchStreamsA, twitchStreamsB);
 }
