cd7390e83cdedc8e4b55d6c83e1273cad0c460d5
Kerry <kerry.convery@gmail.com>
2023-03-18 17:52:03 +1100

    will allow cors on the frontend domain

--- server.ts
+++ server.ts
@@ -1,8 +1,13 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import { router as streamsRouter } from "./routes/stream_routes.ts";
 
 const app = new Application()
-
+app.use(
+    oakCors({
+      origin: "https://react-stream-machine.deno.dev"
+    }),
+);
 app.use(streamsRouter.routes())
 app.use(streamsRouter.allowedMethods())
 
